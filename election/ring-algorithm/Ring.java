13e3a2d4b53e86d517ad25976a7db21302deefbc
Sejal Pawar <sejalpawar212@gmail.com>
2019-06-13 00:56:42 +0530

    Create Ring.java

---
+++ election/ring-algorithm/Ring.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.text.*;
+import java.sql.Timestamp;
+class PseudoServer {
+ static ArrayList < ClientHandler > list = new ArrayList < ClientHandler > ();
+ public static void main(String...args) throws IOException {
+  if (args.length != 1) {
+   System.err.println("Usage: java PseudoServer <port number>");
+   System.exit(1);
+  }
+  ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+  //accept new Processes as Clients int noOfClients = 3; for(int i=0; i<noOfClients; i++) { 
+  try {
+   ClientHandler client = new ClientHandler();
+   Socket clientSocket = serverSocket.accept();
+   client.out = new PrintWriter(clientSocket.getOutputStream(), true);
+   client.in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+   client.id = list.size();
+   list.add(client);
+   System.out.println(message("Connection to client " + client.id + " established."));
+  } catch (Exception e) {
+   e.printStackTrace();
+  }
+ }
+ for (int i = 0; i < noOfClients; i++) {
+  list.get(i).out.println(i);
+  Thread t = new Thread(list.get(i));
+  t.start();
+ }
+}
+static String message(String msg) {
+ return "[" + (new SimpleDateFormat("HH:mm:ss").format(new Timestamp(System.currentTimeMillis()))) + "] " + msg;
+}
+}
+class ClientHandler implements Runnable {
+ int id, nextId;
+ public PrintWriter out;
+ public BufferedReader in ;
+ public void run() {
+  String inputLine = null;
+  int size = PseudoServer.list.size();
+  nextId = id + 1 == size ? 0 : id + 1;
+  while (true) {
+   try {
+    while ((inputLine = in .readLine()) == null);
+    System.out.println(message(id + ": " + inputLine));
+    PseudoServer.list.get(nextId).out.println(inputLine);
+   } catch (Exception e) {
+    e.printStackTrace();
+   }
+  }
+ }
+ String message(String msg) {
+  return "[" + (new SimpleDateFormat("HH:mm:ss").format(new Timestamp(System.currentTimeMillis()))) + "] " + msg;
+ }
+}
